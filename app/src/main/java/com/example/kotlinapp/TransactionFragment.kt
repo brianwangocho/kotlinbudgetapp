b5ecefd86de1820c79273572b5abb87e2c417697
wangocho <bwnagocho@gmail.com>
2020-01-07 19:01:37 +0300

    Change ui in transactions

--- app/src/main/java/com/example/kotlinapp/TransactionFragment.kt
+++ app/src/main/java/com/example/kotlinapp/TransactionFragment.kt
@@ -12,12 +12,16 @@ import android.view.ViewGroup
  */
 class TransactionFragment : Fragment() {
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_transaction, container, false)
+
+        var expenses = arrayOf("food","transport","utilities","rent")
+
     }
 
 
